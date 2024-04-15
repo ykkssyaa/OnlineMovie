5eec5c84be6242bc8aad02192fb5ca452b48bf81
ykkssyaa <mr.ender.03@mail.ru>
2024-04-15 19:44:37 +0300

    review models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    movie = models.ForeignKey('movies.Film', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    text = models.TextField(max_length=2048)
+    created = models.DateTimeField(auto_now=True)
+
+
+class Mark(models.Model):
+    movie = models.ForeignKey('movies.Film', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    Value = models.ValueRange(1, 5)
+
